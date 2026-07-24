3e64484ef4a1ba32ee28334e70a2d00e73253400
WaillyMohamed <Wailly1921@hotmail.com>
2026-07-24 18:52:45 -0400

    Finished hasConflict and getInstanceSorted methods in Planner.

--- src/main/java/entity/Planner.java
+++ src/main/java/entity/Planner.java
@@ -5,9 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import entity.task.Task;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Planner {
@@ -15,7 +18,59 @@ public class Planner {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private LocalDate date;
+
+    @NotNull
     private User user;
-    private ArrayList<Task> tasks;
+
+    @NotNull
+    private ArrayList<ScheduledTaskInstance> instances;
+
+    public void addInstance(@NotNull ScheduledTaskInstance instance) {
+        this.instances.add(instance);
+    }
+    public void removeInstance(@NotNull ScheduledTaskInstance instance) {
+        this.instances.remove(instance);
+    }
+    //
+    public List<ScheduledTaskInstance> getInstanceSorted(@NotNull ArrayList<ScheduledTaskInstance> instances) {
+        // We are sorting instances that fall within these cases
+        // 1. Instances with a valid starttime
+        // 2. Instances with null start time
+        // 3. Instances with an equal start time
+        return instances.stream().sorted((a,b) -> {
+            // Case 1: both have a null start time, so keep original order
+            if(a.getStartTime() == null && b.getStartTime() == null) return 0;
+
+            // Case 2: a has no start time, so push a to the end
+            if(a.getStartTime() == null) return 1;
+
+            // Case 3: b has no start time, so push b to the end
+            if(b.getStartTime() == null) return -1;
+
+            // Case 4: Both have start times so sort normally
+            int compareStartTime = a.getStartTime().compareTo(b.getStartTime());
+            if(compareStartTime != 0) return compareStartTime;
+
+            // Case 5: same Start time so sort by end time.
+            return a.getEndTime().compareTo(b.getStartTime());
+        }).toList();
+    }
+
+    public boolean hasConflict(@NotNull ScheduledTaskInstance newInstance) {
+        boolean conflict = false;
+        for (ScheduledTaskInstance existing: this.instances) {
+            // If either instance has no time assigned yet, then we skip
+            if(existing.getStartTime() == null || existing.getEndTime() == null ||
+                    newInstance.getStartTime() == null || newInstance.getEndTime() == null) {continue;}
+
+            // Otherwise we compare
+            conflict = newInstance.getStartTime().isBefore(existing.getEndTime()) &&
+                    newInstance.getEndTime().isAfter(existing.getStartTime());
+            if(conflict){ return true;}
+        }
+        return false;
+    }
+
 }
--- src/main/java/entity/UserAvailability.java
+++ src/main/java/entity/UserAvailability.java
@@ -1,8 +1,24 @@
 package entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalTime;
 
 @Entity
 public class UserAvailability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private LocalTime startTime;
+
+    @NotNull
+    private LocalTime endTime;
+
 
 }
